f323aa907fe605d9c29ffef2a1a9dbd0ab1e0978
Erick Andrews <andrews_erick@yahoo.com>
2024-10-30 14:50:27 +0000

    trying as me

--- bash/deploy_helm.sh
+++ bash/deploy_helm.sh
@@ -13,6 +13,6 @@ sleep 20
 POD_NAME=$(kubectl get pods -n new-test -l "component=webserver" -o jsonpath="{.items[0].metadata.name}")
 
 # Step 4: Start port-forwarding in the background
-nohup kubectl port-forward --address 0.0.0.0 "$POD_NAME" 8080:8080 -n new-test > port-forward.log 2>&1 &
+nohup kubectl port-forward --as eandrews --address 0.0.0.0 "$POD_NAME" 8080:8080 -n new-test > port-forward.log 2>&1 &
 echo "Port forwarding started on port 8080 for pod $POD_NAME."
 
