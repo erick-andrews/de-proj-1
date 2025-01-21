a0c205ad7856735dda86c890bcb7dabe943bd6bb
Erick Andrews <andrews_erick@yahoo.com>
2025-01-21 14:19:26 +0000

    change to k8s pod operator

--- airflow/dags/ingest_ip_data.py
+++ airflow/dags/ingest_ip_data.py
@@ -1,20 +1,24 @@
-from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow import DAG
 from datetime import datetime
 
 with DAG(
-    dag_id="test_s3_and_token_docker",
+    dag_id="test_s3_and_token_k8s",
     default_args={"retries": 1},
     schedule_interval=None,
     start_date=datetime(2025, 1, 1),
     catchup=False,
 ) as dag:
 
-    # DockerOperator to run the script
-    run_script_task = DockerOperator(
+    # KubernetesPodOperator to replace DockerOperator
+    run_script_task = KubernetesPodOperator(
         task_id="run_script",
+        namespace="new-test",  # Replace with your Kubernetes namespace
         image="erickandrews/bronze_etl:0.1",
-        command="python /src/retrieve_write_parquet.py",
-        docker_url="unix://var/run/docker.sock",
-        network_mode="bridge"
+        cmds=["python"],
+        arguments=["/src/retrieve_write_parquet.py"],
+        name="run-script-task",
+        is_delete_operator_pod=True,  # Delete pod after task completion
+        in_cluster=True,  # Use Kubernetes cluster configuration
+        get_logs=True,  # Stream logs to Airflow
     )
